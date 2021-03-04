f8e4477a1b16de65d36acbbe56011a2256a77453
Emmanuel Obute <emmanuel6.obute@gmail.com>
2021-03-04 16:45:38 +0100

    Add Sholes City

---
+++ property/preview/sholes_city/index.php
@@ -0,0 +1,7 @@
+<?php
+  $title = "Sholes City - ASO Savings &amp; Loans, PLC";
+  
+  include('../../../resources/templates/header.html');
+  include('../../../resources/templates/product-preview/sholes_city_2bed.html');
+  include('../../../resources/templates/footer.html');
+?>

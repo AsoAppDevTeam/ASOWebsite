e0d2aec66b06eff0cc04a8c39f64de71a0ceff3b
Emmanuel Obute <emmanuel6.obute@gmail.com>
2021-07-23 17:27:57 +0100

    Add news and ED profile

---
+++ media/2021/index.php
@@ -0,0 +1,7 @@
+<?php
+  $title = "News Room 2021 - ASO Savings &amp; Loans, PLC";
+  
+  include('../../resources/templates/header.html');
+  include('../../resources/templates/media-news2021.html');
+  include('../../resources/templates/footer.html');
+?>

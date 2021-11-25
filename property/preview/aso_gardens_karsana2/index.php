41bdbdf6934c77030f2828297655fe3b1b63b37a
Chinedu K. Eke <ekechinedu488@gmail.com>
2021-11-25 13:07:18 +0100

    added aso garden terrace property

---
+++ property/preview/aso_gardens_karsana2/index.php
@@ -0,0 +1,7 @@
+<?php
+  $title = "ASO Garden Estate, Karsana - ASO Savings &amp; Loans, PLC";
+  
+  include('../../../resources/templates/header.html');
+  include('../../../resources/templates/product-preview/aso_garden_estate3_karsana.html');
+  include('../../../resources/templates/footer.html');
+?>

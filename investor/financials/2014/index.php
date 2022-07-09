b1540e2137b81f52c29788dac2d2d3b7a8b9e154
Chinedu K. Eke <ekechinedu488@gmail.com>
2022-07-09 04:11:47 +0100

    added 2014 annual report

---
+++ investor/financials/2014/index.php
@@ -0,0 +1,7 @@
+<?php
+  $title = "2014 Financial Statements - ASO Savings &amp; Loans, PLC";
+  
+  include('../../../resources/templates/header.html');
+  include('../../../resources/templates/inv-report2014.html');
+  include('../../../resources/templates/footer.html');
+?>

c60e41323cd39faeb9e47c8dbb58529af82575f9
AsoAppDevTeam <70947030+AsoAppDevTeam@users.noreply.github.com>
2024-07-18 14:07:09 +0100

    Update index.php
    'Removed name of Director Daniel Kunle from the management side bar'

--- company/management/adedigba/index.php
+++ company/management/adedigba/index.php
@@ -2,6 +2,6 @@
   $title = "Adekunle Demola Adedigba - ASO Savings &amp; Loans, PLC";
   
   include('../../../resources/templates/header.html');
-  include('../../../resources/templates/comp-team-adedigba2.html');
+  // include('../../../resources/templates/comp-team-adedigba2.html');
   include('../../../resources/templates/footer.html');
 ?>
